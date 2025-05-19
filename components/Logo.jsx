601e9a30f99efa489caa676548f7b179630cc20b
naeemmahmud70 <naeemmahmud370@gamil.com>
2025-05-19 15:41:01 +0600

    completed

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,18 +1,14 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <Link href="/">
-      <Image
-        className="max-w-[100px] md:max-w-[165px]"
-        src="/lws_logo.png"
-        alt="Lws"
-        height={100}
-        width={165}
-      />
+      <h1 className="font-poppins font-bold text-3xl">
+        Natural <br />
+        Photo Feed
+      </h1>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
